2e047ca1b0550d1cea988e6640a61a43cd920152
Benjamin Rutledge <ben.d.rutledge@gmail.com>
2021-02-07 19:06:44 +0000

    adds returnExpiryDate util function

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
 const { it } = require('mocha');
 const {
-    formatPostcode
+    formatPostcode,
+    returnExpiryDate
 } = require("../utils");
 
 describe('formatPostcode', () => {
@@ -26,5 +27,27 @@ describe('formatPostcode', () => {
         expect(output).to.equal('L1 2EQ');
         const output2 = formatPostcode('LS28 4ep');
         expect(output2).to.equal('LS28 4EP');
+    });
+});
+
+describe('', () => {
+    it('returns a date when passed a date', () => {
+        const input = new Date();
+        const output = returnExpiryDate(input);
+        expect(output).to.be.a('date');
+    });
+    it('returns a date when passed no date', () => {
+        const output = returnExpiryDate();
+        expect(output).to.be.a('date');
+    });
+    it('returns a date 3 years from date when passed date', () => {
+        const output = returnExpiryDate(new Date(2000, 0, 1, 0, 0, 0, 0));
+        expect(output).to.eql(new Date(2003, 0, 1, 0, 0, 0, 0));
+    });
+    it('returns a date 3 years from today\'s date when not passed a date', () => {
+        const today = new Date();
+        const threeYearsFromTodaysDate = new Date(today.setFullYear(today.getFullYear() + 3));
+        const output = returnExpiryDate();
+        expect(output.setHours(0,0,0,0)).to.eql(threeYearsFromTodaysDate.setHours(0,0,0,0));
     });
 });
--- utils/index.js
+++ utils/index.js
@@ -4,3 +4,10 @@ exports.formatPostcode = (postcode) => {
     parts.shift();
     return parts.join(' ');
 }
+
+exports.returnExpiryDate = (date) => {
+    const today = new Date();
+    if (!date) return new Date(today.setFullYear(today.getFullYear() + 3));
+    return new Date(date.setFullYear(date.getFullYear() + 3));
+}
+
