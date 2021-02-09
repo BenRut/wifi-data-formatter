d90fbd5351559c96d1822a31ff2b86c7971525d3
Benjamin Rutledge <ben.d.rutledge@gmail.com>
2021-02-09 10:16:29 +0000

    removes spike.js, now with working basic functionality, from gitignore

---
+++ spike.js
@@ -0,0 +1,73 @@
+// const data = require("./test-data/test-file-asi.csv");
+// const csv = require('csv-parser');
+// const results = [];
+
+// fs.createReadStream('data.csv')
+//   .pipe(csv())
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
+//     console.log(results);
+//   });
+const fs = require('fs');
+const {
+    formatPostcode,
+    returnExpiryDate,
+    getDataByCenter,
+    getCentres
+} = require("./utils");
+const Papa = require('papaparse')
+
+// let data;
+const filePath = './test-data/test-file-l-and-g.csv';
+const csv = require('csvtojson');
+const getData = async () => {
+    const data = await csv().fromFile(filePath);
+    return data;   
+};
+
+
+getData().then(data => {
+    const formattedData = data.map(datum => {
+        datum['Postcode'] = formatPostcode( datum['Postcode']);
+        if (datum['Week Ending']) {
+            datum['Expiry Date'] = returnExpiryDate(datum['Week Ending']);
+        };
+        if (!datum['Week Ending']) {
+            datum['Expiry Date'] = returnExpiryDate();
+        }
+        return datum;
+    })
+    const centres = getCentres(data);
+    for (let i = 0; i < centres.length; i++) {
+        fs.writeFile(`${centres[i]}.csv`, Papa.unparse(formattedData.filter((datum) => {
+            return datum["Registration Location Name"] === centres[i];
+        })), (err) => {
+            if (err) throw err;
+            console.log('CSV saved!');
+        });
+    }
+});
+
+
+
+
+
+
+
+// csv()
+// .fromFile(filePath)
+// .then((data)=>{
+//    data.map(datum => {
+//         datum['Postcode'] = formatPostcode(datum['Postcode']);
+//         console.log(datum);
+//     });
+// });
+
+// constdata = await csv()
+// .fromFile(filePath);
+
+// const formattedPostcodes = data.map(datum => {
+//     console.log(datum);
+// });
+
+
